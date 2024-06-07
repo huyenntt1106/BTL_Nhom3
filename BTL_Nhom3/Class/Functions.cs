4bedfaff7ae5decd900e6009f3585518e4089b76
huyenntt1106 <165869068+huyenntt1106@users.noreply.github.com>
2024-06-08 04:23:19 +0700

    Update Functions.cs

--- BTL_Nhom3/Class/Functions.cs
+++ BTL_Nhom3/Class/Functions.cs
@@ -1,12 +1,228 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
 
-namespace BTL_Nhom3.Class
+namespace BTL.Class
 {
     internal class Functions
     {
+        public static SqlConnection Conn;
+        public static string connstring;
+        public static void ketnoi()
+        {
+            connstring = "Data Source=DESKTOP-0BKJQ7L\\MAY1;Initial Catalog=BaiTapLon;Integrated Security=True;Encrypt=False";
+            Conn = new SqlConnection();
+            Conn.ConnectionString = connstring;
+            Conn.Open();
+            // MessageBox.Show("Done");
+        }
+        public static void Disconnect()
+        {
+            if (Conn.State == ConnectionState.Open)
+            {
+                Conn.Close();
+                Conn.Dispose();
+                Conn = null;
+            }
+        }
+        public static DataTable GetDataToTable(string sql)
+        {
+            SqlDataAdapter Mydata = new SqlDataAdapter();
+            Mydata.SelectCommand = new SqlCommand();
+            Mydata.SelectCommand.Connection = Functions.Conn;
+            Mydata.SelectCommand.CommandText = sql;
+            DataTable table = new DataTable();
+            Mydata.Fill(table);
+            return table;
+
+        }
+        public static void RunSql(string sql)
+        {
+            SqlCommand cmd;
+            cmd = new SqlCommand();
+            cmd.Connection = Class.Functions.Conn;
+            cmd.CommandText = sql;
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            cmd.Dispose();
+            cmd = null;
+        }
+        public static void FillCombo(string sql, ComboBox cbo, string ma)
+        {
+            SqlDataAdapter Mydata = new SqlDataAdapter(sql, Functions.Conn);
+            DataTable table = new DataTable();
+            Mydata.Fill(table);
+            cbo.DataSource = table;
+            cbo.ValueMember = ma;
+        }
+        public static void FillCombo2(string sql, ComboBox cbo, string ma, string ten)
+        {
+            SqlDataAdapter Mydata = new SqlDataAdapter(sql, Functions.Conn);
+            DataTable table = new DataTable();
+            Mydata.Fill(table);
+            cbo.DataSource = table;
+            cbo.ValueMember = ma;
+            cbo.DisplayMember = ma;
+        }
+        public static void FillCombo1(string sql, ComboBox cbo, string ma, string ten)
+        {
+            SqlDataAdapter Mydata = new SqlDataAdapter(sql, Functions.Conn);
+            DataTable table = new DataTable();
+            Mydata.Fill(table);
+            cbo.DataSource = table;
+
+            cbo.ValueMember = ma;    // Truong gia tri
+            cbo.DisplayMember = ten;    // Truong hien thi
+        }
+        public static string GetFieldValues(string sql)
+        {
+            string ma = "";
+            SqlCommand cmd = new SqlCommand(sql, Functions.Conn);
+            SqlDataReader reader;
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                ma = reader.GetValue(0).ToString();
+            }
+            reader.Close();
+            return ma;
+        }
+        public static bool CheckKey(string sql)
+        {
+            SqlDataAdapter mydata = new SqlDataAdapter(sql, Class.Functions.Conn);
+            DataTable table = new DataTable();
+            mydata.Fill(table);
+            if (table.Rows.Count > 0)
+                return true;
+            else
+                return false;
+        }
+        public static bool IsDate(string d)
+        {
+            string[] parts = d.Split('/');
+            if ((Convert.ToInt32(parts[0]) >= 1) && (Convert.ToInt32(parts[0]) <= 31) && (Convert.ToInt32(parts[1]) >= 1) && (Convert.ToInt32(parts[1]) <= 12) && (Convert.ToInt32(parts[2]) >= 1900))
+                return true;
+            else
+                return false;
+        }
+        public static string ConvertDateTime(string d)
+        {
+            string[] parts = d.Split('/');
+            string dt = String.Format("{0}/{1}/{2}", parts[1], parts[0], parts[2]);
+            return dt;
+        }
+        public static double? GetFieldValues1(string sql)
+        {
+            double? ma = null;
+            SqlCommand cmd = new SqlCommand(sql, Functions.Conn);
+            SqlDataReader reader;
+            reader = cmd.ExecuteReader();
+
+            if (reader.Read())
+            {
+                if (!reader.IsDBNull(0))
+                {
+                    ma = reader.GetDouble(0);
+                }
+            }
+
+            reader.Close();
+            return ma;
+        }
+        public static void RunSqlDel(string sql)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = Functions.Conn;
+            cmd.CommandText = sql;
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch (System.Exception)
+            {
+                MessageBox.Show("Dữ liệu đang được dùng, không thể xóa...", "Thông báo",
+MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+            cmd.Dispose();
+        }
+
+        public static string CreateKey(string tiento)
+        {
+            string key = tiento;
+            string[] partsDay;
+            partsDay = DateTime.Now.ToShortDateString().Split('/');
+            //Ví dụ 07/08/2009
+            string d = String.Format("{0}{1}{2}", partsDay[0], partsDay[1], partsDay[2]);
+            key = key + d;
+            string[] partsTime;
+            partsTime = DateTime.Now.ToLongTimeString().Split(':');
+            //Ví dụ 7:08:03 PM hoặc 7:08:03 AM
+            if (partsTime[2].Substring(3, 2) == "PM")
+                partsTime[0] = ConvertTimeTo24(partsTime[0]);
+            if (partsTime[2].Substring(3, 2) == "AM")
+                if (partsTime[0].Length == 1)
+                    partsTime[0] = "0" + partsTime[0];
+            //Xóa ký tự trắng và PM hoặc AM
+            partsTime[2] = partsTime[2].Remove(2, 3);
+            string t;
+            t = String.Format("{0}{1}{2}", partsTime[0], partsTime[1], partsTime[2]);
+            key = key + t;
+            return key;
+        }
+        public static string ConvertTimeTo24(string hour)
+        {
+            string h = "";
+            switch (hour)
+            {
+                case "1":
+                    h = "13";
+                    break;
+                case "2":
+                    h = "14";
+                    break;
+                case "3":
+                    h = "15";
+                    break;
+                case "4":
+                    h = "16";
+                    break;
+                case "5":
+                    h = "17";
+                    break;
+                case "6":
+                    h = "18";
+                    break;
+                case "7":
+                    h = "19";
+                    break;
+                case "8":
+                    h = "20";
+                    break;
+                case "9":
+                    h = "21";
+                    break;
+                case "10":
+                    h = "22";
+                    break;
+                case "11":
+                    h = "23";
+                    break;
+                case "12":
+                    h = "0";
+                    break;
+            }
+            return h;
+        }
     }
 }
